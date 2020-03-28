35e6baaaa6f2cd204aacd75c00d9a3794f274897
Jack Weiler <jackweiler@Jacks-MacBook-Pro-3.local>
2020-03-27 22:05:48 -0400

    got class-to-class imports working

--- App.jsx
+++ App.jsx
@@ -1,30 +1,16 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10,
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "#F5FCFF",
+//   },
+// });
 
 function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-    </View>
-  );
+  return <HomeScreen />;
 }
 export default App;
---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>Welcome to React Native</Text>
+    </View>
+  );
+}
+
+export default HomeScreen;
