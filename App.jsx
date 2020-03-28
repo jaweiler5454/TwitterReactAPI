77cd8632013d4323694b1960c7b7386c616f001b
Jack Weiler <jackweiler@Jacks-MacBook-Pro-3.local>
2020-03-27 22:06:19 -0400

    got class-to-class imports working updated

--- App.jsx
+++ App.jsx
@@ -1,15 +1,6 @@
 import React from "react";
 import HomeScreen from "./screens/HomeScreen";
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//     backgroundColor: "#F5FCFF",
-//   },
-// });
-
 function App() {
   return <HomeScreen />;
 }
