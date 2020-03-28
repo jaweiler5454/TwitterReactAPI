d1e2396cd113a2dff24e2ce03f589cd102cb3c66
Jack Weiler <jackweiler@Jacks-MacBook-Pro-3.local>
2020-03-28 00:15:34 -0400

    working cards from template

--- App.jsx
+++ App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { StyleSheet } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#eaeaea",
+  },
+});
+
 function App() {
-  return <HomeScreen />;
+  return <HomeScreen style={styles.container} />;
 }
 export default App;
